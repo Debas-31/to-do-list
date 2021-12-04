68bb82bcb872137e261350594063e7a0e9f5a3fc
Debas Gebreslasie <65129228+Debas-31@users.noreply.github.com>
2021-12-04 13:09:19 +0300

    Add functionality on add , remove , clear and update on data.js file

--- src/data.js
+++ src/data.js
@@ -1,33 +1,122 @@
-const listArray = [
-  {
-    description: 'Complete To-Do list',
-    completed: false,
-    index: 0,
-  },
-  {
-    description: 'Take a walk',
-    completed: false,
-    index: 1,
-  },
-  {
-    description: 'Watch a movie',
-    completed: false,
-    index: 2,
-  },
-];
+/* eslint-disable no-use-before-define */
 
+const listArray = [];
+const form = document.getElementById('form-id');
 const showTasks = () => {
   const container = document.querySelector('.task-list');
 
-  for (let i = 0; i < listArray.length; i += 1) {
-    const item = listArray[i];
-    const listItem = document.createElement('li');
-    listItem.className = 'list-item';
-    listItem.innerHTML = `<input class="list-input" type = "checkbox"><p class="item-details">${item.description}</p> <label><i class="fas fa-ellipsis-v"></i></label>`;
-    container.appendChild(listItem);
-  }
-  const listInput = document.querySelectorAll('.list-input');
-  return listInput;
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const task = form.element[0].value;
+    const data = {
+      description: task,
+      completed: false,
+      index: listArray.length + 1,
+    };
+    form.reset();
+    listArray.push(data);
+    localStorage.setItem('itemsLocal', JSON.stringify(listArray));
+    add();
+  })
+
 };
 
-export { listArray, showTasks };
+const removeItem = () => {
+  const button = document.querySelectorAll('.fa-trash-alt');
+  button.forEach((item) => {
+    const parent =item.parentNode;
+    const superParent = parent.parentNode;
+    const index = Array.prototype.indexOf.call(superParent.children, parent);
+    const listInput = parent.firstChild;
+    item.addEventListener('click', () => {
+      const itemsLocal = JSON.parse(localStorage.getItem('itemsLocal'));
+      listArray.splice(0, listArray.length, ...itemsLocal);
+      if(listInput.hasAttribute('checked')){
+        parent.remove();
+        listArray.splice(index, 1)
+      }
+      for(let i = 0; i < listArray.length; i += 1)
+      localStorage.setItem('itemsLocal', JSON.stringify(listArray));
+      add();
+    });
+  });
+};
+
+const clearList = () => {
+  const getClearElement = document.querySelector('.clear-task');
+  getClearElement.addEventListener('click', () => {
+    for(let i = 0; i < listArray; i += 1){
+      if(listArray[i].completed){
+        listArray.splice(i, 1);
+      }
+    }
+    for(let i = 0; i < listArray.length; i += 1){
+      listArray[i].index = i + 1;
+    }
+    localStorage.setItem('itemsLocal', JSON.stringify(listArray));
+    add();
+  });
+}
+
+const updateValues = () => {
+  const itemDetails = document.querySelectorAll('item-details');
+  itemDetails.forEach((item) =>{
+    const parent = item.parentNode;
+    const superParent = parent.parentNode;
+    const index = Array.prototype.indexOf.call(superParent.children, parent);
+    item.addEventListener('change', () => {
+      listArray[index].description = item.value;
+      localStorage.setItem('itemsLocal', JSON.stringify(listArray));
+    });
+  });
+};
+
+const textDecor = (listInput) => {
+  listInput.forEach((item) => {
+    if(item.hasAttribute('checked')){
+      item.nextSibling.style.textDecoration = 'line-through'
+    }else{
+      item.nextSibling.style.textDecoration = 'none';
+    }
+  });
+};
+
+export default function populateStorage(){
+  window.addEventListener('load', () => {
+    add();
+    const listInput = document.querySelectorAll('.list-input');
+    const itemsLocal = JSON.parse(localStorage.getItem('itemsLocal'));
+    listInput.forEach((item) => {
+      const parent = item.parentNode;
+      const superParent = parent.parentNode;
+      const index = Array.prototype.indexOf.call(superParent.children, parent);
+      const currentItem = itemsLocal[index].completed;
+      if(currentItem){
+        item.setAttribute('checked', '');
+        parent.lastChild.style.display = 'block';
+      }
+    });
+    textDecor(listInput);
+  });
+}
+
+const add = () => {
+  const list = document.querySelector('.task-list');
+  const itemsLocal = JSON.parse(localStorage.getItem('itemsLocal'));
+  listArray.splice(0, listArray.length, ...itemsLocal);
+  list.innerHTML = '';
+  for (let i = 0; i < listArray.length; i += 1){
+    const { description } = itemsLocal[i];
+    const listItem = document.createElement('li');
+    listItem.className = 'list-item';
+    listItem.innerHTML = `<input class="list-input" type="checkbox"><textarea name="textarea cols="30" class="item-details">${description}</textarea><i class="fas fa-trash-alt"></i>`;
+    list.appendChild(listItem);
+  }
+  const listInput = document.querySelectorAll('.list-input');
+  interact(listInput);
+  clearList(listInput);
+  removeItem();
+  updateValues();
+}
+
+showTasks();
